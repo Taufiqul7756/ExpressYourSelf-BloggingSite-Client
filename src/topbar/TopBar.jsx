1456b0a849e2f9c6ee21a0beb12ca7b330b2cc38
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-12-22 19:10:43 +0600

    Header Part 30% Done

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import "./TopBar.css";
+
+export default function TopBar() {
+  return (<div className="top">
+        <div className="topLeft">
+        <i class="fa-brands fa-square-facebook"></i>
+        </div>
+        <div className="topCenter"> C</div>
+        <div className="topRight"> R</div>
+    </div>
+  )
+}
