0a4eb8cce3391c635cc2168c1c8bb95d90f1bb72
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-12-26 17:15:38 +0600

    Instagram and Youtube logo added

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -5,8 +5,9 @@ export default function TopBar() {
         <div className="topLeft">
         <i class="fa-brands fa-square-facebook"></i>
         </div>
-        <div className="topCenter"> C</div>
-        <div className="topRight"> R</div>
+        <div className="topCenter"> 
+        <i class="fa-brands fa-instagram"></i></div>
+        <div className="topRight"> <i class="fa-brands fa-youtube"></i></div>
     </div>
   )
 }
