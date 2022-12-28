5e1706a2cb4064edb18b9be17afbfaa44d42c4bc
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-12-28 23:56:38 +0600

    NavBar added

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,13 +4,23 @@ export default function TopBar() {
   return (<div className="top">
     <div className="topLeft">
       <i class="fa-brands fa-square-facebook"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-youtube"></i>
+      <i class="fa-brands fa-twitter"></i>
     </div>
     <div className="topCenter">
-      <i class="fa-brands fa-instagram"></i></div>
-    <div className="topRight"> <i class="fa-brands fa-youtube"></i></div>
-    <p>Test</p>
-    <p>Test</p>
-    <p>Test</p>
+      <ul className="topList">
+        <li className="topListItem">HOME</li>
+        <li className="topListItem">ABOUT</li>
+        <li className="topListItem">CONTACT</li>
+        <li className="topListItem">WRITE</li>
+        <li className="topListItem">LOGOUT</li>
+      </ul>
+      </div>
+    <div className="topRight"> 
+    <img src="https://en.wikipedia.org/wiki/Portrait_photography#/media/File:ECurtis.jpg" alt="" />
+    <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
   </div>
   )
 }
